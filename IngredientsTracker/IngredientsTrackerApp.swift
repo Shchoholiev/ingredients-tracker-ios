ef30599dac83585ae6f3c5aa489a5ad984828b38
Shchoholiev <sh.codework@gmail.com>
2024-05-18 11:24:42 -0400

    Initial Commit

---
+++ IngredientsTracker/IngredientsTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  IngredientsTrackerApp.swift
+//  IngredientsTracker
+//
+//  Created by Serhii Shchoholiev on 5/18/24.
+//
+
+import SwiftUI
+
+@main
+struct IngredientsTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
