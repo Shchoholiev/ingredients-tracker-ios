ab948eb01dd9e2bf7c55ac086d0b31b30e9fe2d8
Shchoholiev <sh.codework@gmail.com>
2024-05-18 21:55:41 -0400

    Add recipe cooking logic

---
+++ IngredientsTracker/Views/RecipeDetailsView.swift
@@ -0,0 +1,18 @@
+//
+//  RecipeDetailsView.swift
+//  IngredientsTracker
+//
+//  Created by Serhii Shchoholiev on 5/18/24.
+//
+
+import SwiftUI
+
+struct RecipeDetailsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RecipeDetailsView()
+}
